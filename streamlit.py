78ca1af4e326b43664d67d2e5f3450459bfcb6f0
mihuwa2009 <quangminhhuy2009@gmail.com>
2024-06-10 16:31:34 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -3,13 +3,19 @@ import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from PIL import Image
 
-canvas_result = st_canvas(stroke_width=15,
-						  stroke_color='rgb(255, 255, 255)',
-						  background_color='rgb(0, 0, 0)',
-						  height=150,
-						  width=150,
-						  key="canvas")
+tabs = st.tabs(["Model Training", "Drawable Canvas"])
 
-if canvas_result.image_data is not None:
-  img = canvas_result.image_data
-  st.write(img.shape)
+with tabs[0]:
+	st.header('Test')
+
+with tabs[1]:
+	canvas_result = st_canvas(stroke_width=15,
+							  stroke_color='rgb(255, 255, 255)',
+							  background_color='rgb(0, 0, 0)',
+							  height=150,
+							  width=150,
+							  key="canvas")
+	
+	if canvas_result.image_data is not None:
+	  img = canvas_result.image_data
+	  st.write(img.shape)
