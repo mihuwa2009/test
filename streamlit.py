cf759fafa6bec2ee4b19f3ae37aec357b06fa0f8
mihuwa2009 <quangminhhuy2009@gmail.com>
2024-06-10 16:36:57 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit_drawable_canvas import st_canvas
 
 # Sidebar with navigation options
 st.sidebar.title("Navigation")
@@ -14,10 +15,19 @@ elif page == "Training Models":
     st.title("Training Models")
     st.write("Here you can train your models.")
     # Add model training widgets and functionality here
-
+    
 # Results Page
 elif page == "Results":
     st.title("Results")
     st.write("Here are the results.")
     # Add results display widgets and functionality here
+    canvas_result = st_canvas(stroke_width=15,
+						  stroke_color='rgb(255, 255, 255)',
+						  background_color='rgb(0, 0, 0)',
+						  height=150,
+						  width=150,
+						  key="canvas")
 
+    if canvas_result.image_data is not None:
+      img = canvas_result.image_data
+      st.write(img.shape)
