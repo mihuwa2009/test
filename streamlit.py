5f76109403d959921a83f9898aea71c706449060
mihuwa2009 <quangminhhuy2009@gmail.com>
2024-06-11 11:23:54 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,33 +1,27 @@
 import streamlit as st
-from streamlit_drawable_canvas import st_canvas
+from streamlit_option_menu import option_menu
 
-# Sidebar with navigation options
-st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Home", "Training Models", "Results"])
+# Sidebar with tabbed navigation
+with st.sidebar:
+    selected = option_menu(
+        "Main Menu", ["Home", "Training Models", "Results"],
+        icons=['house', 'gear', 'bar-chart'],
+        menu_icon="cast", default_index=0,
+    )
 
 # Home Page
-if page == "Home":
+if selected == "Home":
     st.title("Home")
     st.write("Welcome to the Home page.")
 
 # Training Models Page
-elif page == "Training Models":
+elif selected == "Training Models":
     st.title("Training Models")
     st.write("Here you can train your models.")
     # Add model training widgets and functionality here
-    
+
 # Results Page
-elif page == "Results":
+elif selected == "Results":
     st.title("Results")
     st.write("Here are the results.")
     # Add results display widgets and functionality here
-    canvas_result = st_canvas(stroke_width=15,
-						  stroke_color='rgb(255, 255, 255)',
-						  background_color='rgb(0, 0, 0)',
-						  height=150,
-						  width=150,
-						  key="canvas")
-
-    if canvas_result.image_data is not None:
-      img = canvas_result.image_data
-      st.write(img.shape)
